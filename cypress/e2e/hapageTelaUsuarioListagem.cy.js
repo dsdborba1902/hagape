544fd556e39e7d00f3c40fd3d5172e74684ebcef
Diogo Borba <dsdborba@gmail.com>
2023-11-14 16:14:18 -0300

    Testes com menu usuário ok

--- cypress/e2e/hapageTelaUsuarioListagem.cy.js
+++ cypress/e2e/hapageTelaUsuarioListagem.cy.js
@@ -15,24 +15,30 @@ describe('Teste de login e logout', () => {
     cy.contains('Listagem').should('be.visible');
     cy.get(':nth-child(3) > .MuiCollapse-root > .MuiCollapse-wrapper > .MuiCollapse-wrapperInner > :nth-child(1) > .MuiButtonBase-root > .MuiBox-root > .MuiTypography-root').click();
 
-    //Validar campos da tela
+    //Validar título da página
     cy.get('.MuiCardHeader-content > .MuiTypography-root').should('be.visible');
 
     //Validar campo E-mail
+    cy.get('[tabindex="0"] > .MuiDataGrid-columnHeaderDraggableContainer > .MuiDataGrid-columnHeaderTitleContainer > .MuiDataGrid-columnHeaderTitleContainerContent > .MuiDataGrid-columnHeaderTitle').should('be.visible');
+
+    //Validar campo Nome
+    cy.get('[style="height: 56px; width: 473.333px; min-width: 473.333px; max-width: 473.333px;"] > .MuiDataGrid-columnHeaderDraggableContainer > .MuiDataGrid-columnHeaderTitleContainer > .MuiDataGrid-columnHeaderTitleContainerContent > .MuiDataGrid-columnHeaderTitle').should('be.visible');
+
+    //Validar campo Status
+    cy.get('[aria-colindex="3"] > .MuiDataGrid-columnHeaderDraggableContainer > .MuiDataGrid-columnHeaderTitleContainer > .MuiDataGrid-columnHeaderTitleContainerContent > .MuiDataGrid-columnHeaderTitle').should('be.visible');
+
+    //Validar campo Ações
+    cy.get('[style="height: 56px; width: 100px; min-width: 100px; max-width: 100px;"] > .MuiDataGrid-columnHeaderDraggableContainer > .MuiDataGrid-columnHeaderTitleContainer > .MuiDataGrid-columnHeaderTitleContainerContent > .MuiDataGrid-columnHeaderTitle').should('be.visible');
+
+    //Validar botão Adicionar Usuário
+    cy.get('.css-t5y380 > .MuiBox-root > .MuiButtonBase-root').click();
 
 
-    //Validar
-    
-
-
-
-
-
-  //logout
+    //logout
     //cy.get('.nav-header > .MuiButtonBase-root').click(); 
-    //cy.get('.MuiAvatar-img').click();
-    //cy.get('.css-ipchd0').click();
-    //cy.get('.MuiTypography-h6').should('be.visible');  
+    cy.get('.MuiAvatar-img').click();
+    cy.get('.css-ipchd0').click();
+    cy.get('.MuiTypography-h6').should('be.visible');  
 
   })
   
