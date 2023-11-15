0db9740ed10448cf1d7c3008f62e1943e58a0820
Diogo Borba <dsdborba@gmail.com>
2023-11-15 18:00:07 -0300

    confiMOcha

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,15 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
+  video: true,
+  charts: true,
+  overwrite: false,
+  html: true,
+  json: true,
+  embeddedScreenshots: false,
+  saveAllAttempts: true,
+  reportDir: 'cypress/report/mochawesome-report',
+
   e2e: {
     defaultCommandTimeout: 10000,
 
