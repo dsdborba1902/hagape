91342533823a16ccc8717d7dedb5f4b14e7e1fd4
Diogo Borba <dsdborba@gmail.com>
2023-12-12 10:31:08 -0300

    Pesquisa Gratuidade completo

--- cypress/e2e/hagapePesqGratuidade.cy.js
+++ cypress/e2e/hagapePesqGratuidade.cy.js
@@ -17,15 +17,22 @@ describe('Teste Menu Pesquisa Gratuidade', () => {
   cy.get('.MuiGrid-grid-xs-3 > .demo-space-x > .MuiButtonBase-root').should('be.visible');
   
   //Campo Pesquisar Gratuidade
+  cy.get('h4').should('be.visible');
+  cy.get('.css-9kcp2 > h3').should('be.visible');
+  cy.get('.MuiGrid2-container > :nth-child(2)').should('be.visible');
+  cy.get(':nth-child(3) > .MuiFormControl-fullWidth').should('be.visible');
+  cy.get('.MuiGrid2-grid-md-7').should('be.visible');
+  cy.get('.MuiGrid2-grid-md-5').should('be.visible');
 
+  //Botões de ação
+  cy.get('.MuiGrid-grid-md-12 > .demo-space-x > .MuiButton-contained').should('be.visible');
+  cy.get('.MuiButton-outlined').should('be.visible');
 
+  //Logoff
+  cy.get('.MuiAvatar-img').click();  
+  cy.get('.css-ipchd0').click();
+    cy.get('.MuiTypography-h6').should('be.visible');
 
-
-
-
-
-    //cy.realizarLogoff();   
-  
-  })
+      })
   
 })
