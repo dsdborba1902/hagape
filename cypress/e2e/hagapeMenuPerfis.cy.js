785ec3444361f9ab33ca4893daad8b057faffadd
Diogo Borba <dsdborba@gmail.com>
2024-01-03 14:13:12 -0300

    Atualizado até o Menu Setor

--- cypress/e2e/hagapeMenuPerfis.cy.js
+++ cypress/e2e/hagapeMenuPerfis.cy.js
@@ -8,12 +8,25 @@ describe('Teste Menu Perfis', () => {
     cy.realizarLogin(baseurl,login);
     cy.get('.actions-left > .MuiButtonBase-root').click();
     cy.get('.css-1t0b3pu > .MuiButtonBase-root > .MuiBox-root > .MuiTypography-root').should('have.text', "Início");
-    cy.get(':nth-child(4) > .css-11coqyv > .css-164kvcj > .MuiTypography-root').click();
-    cy.get(':nth-child(4) > .MuiCollapse-root > .MuiCollapse-wrapper > .MuiCollapse-wrapperInner > :nth-child(1) > .MuiButtonBase-root > .MuiBox-root > .MuiTypography-root').should('be.visible');
-    cy.get(':nth-child(4) > .MuiCollapse-root > .MuiCollapse-wrapper > .MuiCollapse-wrapperInner > :nth-child(2) > .MuiButtonBase-root > .MuiBox-root > .MuiTypography-root').should('be.visible');
+    cy.get(':nth-child(4) > .MuiButtonBase-root > .MuiBox-root > .MuiTypography-root').click();
+   
+    //Validar Nome da tela
+    cy.get('.MuiCardHeader-content > .MuiTypography-root').should('be.visible');
+
+    //Validar Botão Adicionar Perfil
+    cy.get('.css-t5y380 > .MuiBox-root > .MuiButtonBase-root').should('be.visible');
+    
+    //Validar listagem
+    cy.get('[data-id="1"] > [data-field="name"]').should('be.visible');
+    cy.get('[data-id="1"] > [data-field="status"] > :nth-child(1) > .MuiChip-root > .MuiChip-label').should('be.visible');
+    
+    //Validar botão Ações
+    cy.get('[data-id="1"] > [data-field="actions"]').should('be.visible');
+   
   //logoff
-    cy.realizarLogoff();   
-  
+    cy.get('.MuiAvatar-img').click();
+    cy.contains('Sair').click();
+    
   })
   
 })
--- cypress/e2e/hagapeMenuSetor.cy.js
+++ cypress/e2e/hagapeMenuSetor.cy.js
@@ -8,12 +8,27 @@ describe('Teste Menu Setor', () => {
     cy.realizarLogin(baseurl,login);
     cy.get('.actions-left > .MuiButtonBase-root').click();
     cy.get('.css-1t0b3pu > .MuiButtonBase-root > .MuiBox-root > .MuiTypography-root').should('have.text', "Início");
-    cy.get(':nth-child(5) > .css-11coqyv > .css-164kvcj > .MuiTypography-root').click();
-    cy.get(':nth-child(5) > .MuiCollapse-root > .MuiCollapse-wrapper > .MuiCollapse-wrapperInner > :nth-child(1) > .MuiButtonBase-root > .MuiBox-root > .MuiTypography-root').should('be.visible');
-    cy.get(':nth-child(5) > .MuiCollapse-root > .MuiCollapse-wrapper > .MuiCollapse-wrapperInner > :nth-child(2) > .MuiButtonBase-root > .MuiBox-root > .MuiTypography-root').should('be.visible');
+    cy.get(':nth-child(5) > .MuiButtonBase-root > .MuiBox-root > .MuiTypography-root').click();
+
+    //Validar Nome da tela
+    cy.get('.MuiCardHeader-content > .MuiTypography-root').should('be.visible');
+    
+    //Validar Botão adicionar Setor
+    cy.get('.css-t5y380 > .MuiBox-root > .MuiButtonBase-root').should('be.visible');
+    
+    //Validar listagem
+    cy.get('[data-id="1"] > [data-field="nome"] > .MuiTypography-root').should('be.visible');
+    cy.get('[data-id="1"] > [data-field="status"] > :nth-child(1) > .MuiChip-root > .MuiChip-label').should('be.visible');
+
+    //Validar botão Ações
+    cy.get('[data-id="1"] > [data-field="actions"] > .MuiButtonBase-root > .iconify').should('be.extensible');
+
+       
     //logout
-    cy.realizarLogoff();  
-  
+    cy.get('.MuiAvatar-img').click();
+    cy.contains('Sair').click();
+   
+
   })
   
 })
